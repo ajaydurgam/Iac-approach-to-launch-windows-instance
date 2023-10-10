95f8de07820746980e854fafd082c908063f32da
ajaydurgam <137983221+ajaydurgam@users.noreply.github.com>
2023-10-10 19:03:38 +0000

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,15 +1,60 @@
+resource "aws_security_group" "windows" {
+  name        = "windows_security_group"
+  vpc_id      = lookup(var.windowsconfig, "vpc")
+  description = "Allow HTTPS (443) and RDP (3389) traffic, Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH access from anywhere (for demonstration purposes)
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP access from anywhere (for demonstration purposes)
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTPS from anywhere (for demonstration purposes)
+  }
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow RDP from anywhere (for demonstration purposes)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_instance" "project-iac-approach-to-launch-windows" {
   ami                         = lookup(var.windowsconfig, "ami")
   instance_type               = lookup(var.windowsconfig, "itype")
   subnet_id                   = lookup(var.windowsconfig, "subnet")
   associate_public_ip_address = lookup(var.windowsconfig, "publicip")
   key_name                    = lookup(var.windowsconfig, "keyname")
-  vpc_security_group_ids      = [var.windowsconfig.secgroupid]
+  vpc_security_group_ids      = [aws_security_group.windows.id]
 
   tags = {
-    Name = "windows-iac-approach"
+    Name = "windows-iac-approach" #tags used to give a instance name
   }
 }
+
 resource "aws_ebs_volume" "voulme-to-attach-it-to-windows" {
   availability_zone = aws_instance.project-iac-approach-to-launch-windows.availability_zone
   size              = lookup(var.windowsconfig, "size")
@@ -29,10 +74,4 @@ resource "aws_volume_attachment" "ebs_att" {
 output "need-public-ip" {
   value = aws_instance.project-iac-approach-to-launch-windows.public_ip
 }
- triggers = {
-    password = "${rsadecrypt(aws_instance.example.password_data, file("mykey.pem"))}"
-  }
 
-output "Administrator_Password" {
-    value = "${null_resource.example.triggers.password}"
-}
