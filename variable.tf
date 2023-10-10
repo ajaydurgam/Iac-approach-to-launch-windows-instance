210f0dc8c05ac08a664a1f5c0f7c762440c11506
ajaydurgam <137983221+ajaydurgam@users.noreply.github.com>
2023-10-10 22:13:18 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,14 +1,14 @@
 variable "windowsconfig" {
   type = map(string)
   default = {
-    region     = "ap-southeast-2"
-    vpc        = "vpc-0047bb15d5c383ca3"
-    ami        = "ami-0b79ab0b8cfba7ecd"
+    region     = "us-east-1e"
+    vpc        = "vpc-06f584a255a8ccdaf"
+    ami        = "ami-037faf8d69ffcc8ec"
     itype      = "t2.xlarge"
     subnet     = "subnet-0971126764e26e3c8"
     publicip   = true
     keyname    = "default-keypair"
-    secgroupid = "sg-0827481a9ae9805b4"
+    secgroupid = "NA"
     size       = "100"
     type       = "gp2"
     devicename = "/dev/sdh"
